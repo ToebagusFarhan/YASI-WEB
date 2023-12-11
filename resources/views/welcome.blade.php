c00fd653db5f7b59ed0af949cdcf8047e2042263
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2023-12-11 20:57:42 +0700

    test second pull ke server
    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Ini Buat landing page
-        <?php</title>
+    <title>Ini Buat landing page</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
